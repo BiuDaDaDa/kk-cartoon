543e3944622acdeec52abb96c58bd0c937bf0772
dllo <dllo@huojinxin.local>
2017-11-17 21:06:08 +0800

    docs:首页 by HuoJinXin

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,13 @@ import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 // import HansRouters from '../router/kk-hans'
 // import ZwlRouters from './kk-zwl'
-import HuoRouters from '../router/kk-huo'
+// import HuoRouters from '../router/kk-huo'
 // import YuanRouters from './kk-yuan'
 
 // var kkRouters = YuanRouters.concat(HansRouters, HuoRouters, ZwlRouters)
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  routes: HuoRouters
+  mode: 'history'
+  // routes: HuoRouters
 })
