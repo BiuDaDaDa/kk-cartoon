a9d9d1afc7440415807358aed5f535d951802e4a
dllo <dllo@Moriarty.local>
2017-11-21 08:42:44 +0800

    feat #12 by ymy  "我的"页面我的消息部分，touch切换效果

--- src/router/kk-yuan/index.js
+++ src/router/kk-yuan/index.js
@@ -1,8 +1,28 @@
 import CartonUser from '../../components/kk-user/CartonUser'
+import UserNews from '../../components/kk-user/UserNews'
+import Inform from '../../components/kk-user/Inform'
+import Comment from '../../components/kk-user/Comment'
 export default [
   {
     path: '/user',
     name: 'CartonUser',
     component: CartonUser
+  },
+  {
+    path: '/userNews',
+    name: 'UserNews',
+    component: UserNews,
+    children: [
+      {
+        path: '/userNews/inform',
+        name: 'Inform',
+        component: Inform
+      },
+      {
+        path: '/userNews/comment',
+        name: 'Comment',
+        component: Comment
+      }
+    ]
   }
 ]
