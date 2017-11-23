99fb8e7fd253b5e9839e2ee73f40f33e83001831
dllo <dllo@huojinxin.local>
2017-11-23 08:11:08 +0800

    feat:评论 by HuoJinXin

--- config/index.js
+++ config/index.js
@@ -12,52 +12,62 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/v1': {
+      '/kuaikanv1': {
         target: 'http://api.kuaikanmanhua.com',
         pathRewrite: {
-          '^/v1': '/v1'
+          '^/kuaikanv1': '/v1'
         },
         headers: {
           Host: 'api.kuaikanmanhua.com'
         }
       },
-<<<<<<< HEAD
-      '/v2': {
+      '/kuaikanv2': {
         target: 'http://api.kuaikanmanhua.com',
         pathRewrite: {
-          '^/v2': '/v2'
+          '^/kuaikanv2': '/v2'
         },
         headers: {
           Host: 'api.kuaikanmanhua.com'
         }
       },
-      '/cartonUser': {
+      '/kuaikanv3': {
+        target: 'http://api.kuaikanmanhua.com',
+        pathRewrite: {
+          '^/kuaikanv3': '/v3'
+        },
+        headers: {
+          Host: 'api.kuaikanmanhua.com'
+        }
+      },
+      '/kkv1': {
         target: 'https://api.kkmh.com',
-=======
-      '/api': {
-        target: 'http://api.kuaikanmanhua.com',
->>>>>>> f653208b72f459cc5b75943213e9537568b1522b
         pathRewrite: {
-          '^/api': '/v1'
+          '^/kkv1': '/v1'
         },
         headers: {
-          Host: 'api.kuaikanmanhua.com'
+          Host: 'api.kkmh.com'
         }
       },
-      '/v2': {
-        '/cartonUser': {
-          target: 'https://api.kkmh.com',
-          pathRewrite: {
-            '^/v2': '/v2'
-          },
-          headers: {
-            Host: 'api.kkmh.com'
-          }
+      '/kkv2': {
+        target: 'https://api.kkmh.com',
+        pathRewrite: {
+          '^/kkv2': '/v2'
+        },
+        headers: {
+          Host: 'api.kkmh.com'
+        }
+      },
+      '/kkv3': {
+        target: 'https://api.kkmh.com',
+        pathRewrite: {
+          '^/kkv3': '/v3'
+        },
+        headers: {
+          Host: 'api.kkmh.com'
         }
       }
     }
   },
-<<<<<<< HEAD
   build: {
     // Template for index.html
     index: path.resolve(__dirname, '../dist/index.html'),
@@ -74,50 +84,41 @@ module.exports = {
     build: {
       // Template for index.html
       index: path.resolve(__dirname, '../dist/index.html'),
-=======
+      build: {
+        // Template for index.html
+        index: path.resolve(__dirname, '../dist/index.html'),
+        // Paths
+        assetsRoot: path.resolve(__dirname, '../dist'),
+        assetsSubDirectory: 'static',
+        assetsPublicPath: '/',
 
-  build: {
-    // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
->>>>>>> f653208b72f459cc5b75943213e9537568b1522b
+        /**
+         * Source Maps
+         */
 
-    // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+        productionSourceMap: true,
+        // https://webpack.js.org/configuration/devtool/#production
+        devtool: '#source-map',
 
-    /**
-     * Source Maps
-     */
+        // Gzip off by default as many popular static hosts such as
+        // Surge or Netlify already gzip all static assets for you.
+        // Before setting to `true`, make sure to:
+        // npm install --save-dev compression-webpack-plugin
+        productionGzip: false,
+        productionGzipExtensions: ['js', 'css'],
 
-    productionSourceMap: true,
-    // https://webpack.js.org/configuration/devtool/#production
-    devtool: '#source-map',
-
-    // Gzip off by default as many popular static hosts such as
-    // Surge or Netlify already gzip all static assets for you.
-    // Before setting to `true`, make sure to:
-    // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
-    productionGzipExtensions: ['js', 'css'],
-
+        // Run the build command with an extra argument to
+        // View the bundle analyzer report after build finishes:
+        // `npm run build --report`
+        // Set to `true` or `false` to always turn it on or off
+        bundleAnalyzerReport: process.env.npm_config_report
+      }
+    },
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
-<<<<<<< HEAD
-    }
-  },
-      // Run the build command with an extra argument to
-      // View the bundle analyzer report after build finishes:
-      // `npm run build --report`
-      // Set to `true` or `false` to always turn it on or off
-      bundleAnalyzerReport: process.env.npm_config_report
-=======
->>>>>>> f653208b72f459cc5b75943213e9537568b1522b
   }
 
 }
-
-
