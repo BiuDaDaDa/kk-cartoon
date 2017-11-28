a212ef6db1e33252f89f9995c5e9ef4dae12025f
dllo <dllo@12344321.local>
2017-11-28 18:56:39 +0800

    update by zwl:完善代码功能

--- src/components/kk-nav/FooterNav.vue
+++ src/components/kk-nav/FooterNav.vue
@@ -31,34 +31,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-    export default {
-      name: 'FooterNav',
-      data () {
-        return {
-          cartonSrc: require('../../assets/kk-nav/kk-nav-carton.png'),
-          findSrc: require('../../assets/kk-nav/kk-nav-find.png'),
-          clubSrc: require('../../assets/kk-nav/kk-nav-v.png'),
-          userSrc: require('../../assets/kk-nav/kk-nav-user.png')
-        }
-      },
-      mounted () {
-//        console.log(window.location.href.split('8081')[1])
-        let link = window.location.href.split('8081')[1]
-        if (link === '/user') {
-          this.userSrc = require('../../assets/kk-nav/kk-nav-user-c.png')
-          this.$refs.userRef.style.color = '#fce13c'
-        } else if (link === '/' || link === '/attention') {
-          this.cartonSrc = require('../../assets/kk-nav/kk-nav-carton-c.png')
-          this.$refs.cartonRef.style.color = '#fce13c'
-        } else if (link === '/kkFind' || link === '/kkFindFen') {
-          this.findSrc = require('../../assets/kk-nav/kk-nav-find-c.png')
-          this.$refs.findRef.style.color = '#fce13c'
-        } else if (link === '/kk-club') {
-          this.clubSrc = require('../../assets/kk-nav/kk-nav-v-c.png')
-          this.$refs.clubRef.style.color = '#fce13c'
-        }
-=======
   export default {
     name: 'FooterNav',
     data () {
@@ -85,7 +57,6 @@
       } else if (link === '/kk-club' || link === '/kk-attention') {
         this.clubSrc = require('../../assets/kk-nav/kk-nav-v-c.png')
         this.$refs.clubRef.style.color = '#fce13c'
->>>>>>> 9f7ace26df067fe417c3b9afa99840696323e4ba
       }
     }
   }
